f4cbe81d660c78abbf40510a5c702fa52e5f72a2
Hema Koteswar Naidu <hemakotibonthada@gmail.com>
2024-05-26 09:36:54 +0530

    Esp32 Locally Controllable code using HTTP Requests

--- Embedded_Systems/Practice_Esp32/src/main.cpp
+++ Embedded_Systems/Practice_Esp32/src/main.cpp
@@ -1,18 +1,71 @@
-#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
 
-// put function declarations here:
-int myFunction(int, int);
+// Replace with your network credentials
+const char* ssid = "YOUR_SSID";
+const char* password = "YOUR_PASSWORD";
+
+// Set web server port number to 80
+WebServer server(80);
+
+// Define the LED pins
+const int ledPins[10] = {2, 4, 5, 12, 13, 14, 15, 16, 17, 18};
+bool ledStates[10] = {false, false, false, false, false, false, false, false, false, false};
+
+void handleRoot() {
+  String html = "<h1>ESP32 Control</h1>";
+  for (int i = 0; i < 10; i++) {
+    html += "<p>LED " + String(i + 1);
+    if (ledStates[i]) {
+      html += " <a href=\"/off" + String(i) + "\"><button>OFF</button></a></p>";
+    } else {
+      html += " <a href=\"/on" + String(i) + "\"><button>ON</button></a></p>";
+    }
+  }
+  server.send(200, "text/html", html);
+}
+
+void handleLEDOn(int ledIndex) {
+  digitalWrite(ledPins[ledIndex], HIGH);
+  ledStates[ledIndex] = true;
+  server.send(200, "text/html", "<h1>LED " + String(ledIndex + 1) + " is ON</h1><p><a href=\"/\"><button>Back</button></a></p>");
+}
+
+void handleLEDOff(int ledIndex) {
+  digitalWrite(ledPins[ledIndex], LOW);
+  ledStates[ledIndex] = false;
+  server.send(200, "text/html", "<h1>LED " + String(ledIndex + 1) + " is OFF</h1><p><a href=\"/\"><button>Back</button></a></p>");
+}
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  for (int i = 0; i < 10; i++) {
+    pinMode(ledPins[i], OUTPUT);
+    digitalWrite(ledPins[i], LOW);
+  }
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi...");
+  }
+  Serial.println("Connected to WiFi");
+  Serial.print("IP Address: ");
+  Serial.println(WiFi.localIP());
+
+  // Initialize web server routes
+  server.on("/", handleRoot);
+  for (int i = 0; i < 10; i++) {
+    server.on("/on" + String(i), [i]() { handleLEDOn(i); });
+    server.on("/off" + String(i), [i]() { handleLEDOff(i); });
+  }
+
+  // Start server
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  server.handleClient();
 }
